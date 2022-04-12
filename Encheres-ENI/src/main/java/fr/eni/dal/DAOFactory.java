b4b2f5be1cd25829f8d95e770d29171a0d394383
clozachmeur2021 <clozachmeur2021@170SE3-9Q405M2.ad.campus-eni.fr>
2022-04-12 16:39:19 +0200

    effacement jee

--- Encheres-ENI/src/main/java/fr/eni/dal/DAOFactory.java
+++ Encheres-ENI/src/main/java/fr/eni/dal/DAOFactory.java
@@ -2,7 +2,8 @@ package fr.eni.jee.dal;
 
 public class DAOFactory {
 	/**
-	 * Cette méthode sert à éviter le : RepasDAO repasDAO = new RepasDAOJdbcImpl() dans la couche BLL
+	 * Cette méthode sert à éviter le : RepasDAO repasDAO = new RepasDAOJdbcImpl()
+	 * dans la couche BLL
 	 */
 	public static EnchereDAOJdbcImpl getEnchereDAO() {
 		return new EnchereDAOJdbcImpl();
