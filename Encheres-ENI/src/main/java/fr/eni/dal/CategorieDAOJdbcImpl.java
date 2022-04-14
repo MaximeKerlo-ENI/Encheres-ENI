23f583cc02fdbb1d0957df1248f7cb98a22fc41f
clozachmeur2021 <clozachmeur2021@170SE3-9Q405M2.ad.campus-eni.fr>
2022-04-14 19:56:20 +0200

    corrections erreurs

--- Encheres-ENI/src/main/java/fr/eni/dal/CategorieDAOJdbcImpl.java
+++ Encheres-ENI/src/main/java/fr/eni/dal/CategorieDAOJdbcImpl.java
@@ -5,12 +5,15 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import fr.eni.bll.BusinessException;
 import fr.eni.bo.Categorie;
 import fr.eni.bo.Enchere;
 
-public  class CategorieDAOJdbcImpl implements DAOCategorie {
+
+public class CategorieDAOJdbcImpl implements DAOCategorie  {
 	
 	
 	  public void insert(Categorie categorie) throws BusinessException, SQLException {
@@ -52,4 +55,77 @@ public  class CategorieDAOJdbcImpl implements DAOCategorie {
 	        return sql;
 	    }
 
+	  public List<Categorie> selectAll() throws BusinessException, SQLException {
+		  Connection cnx = ConnectionProvider.getConnection();
+	        List<Categorie> categories = new ArrayList<>();
+	        try{
+	        	String SELECT_ALL = "SELECT * FROM CATEGORIES";
+	        	PreparedStatement stmt = cnx.prepareStatement(SELECT_ALL);
+	            
+	            stmt.execute(SELECT_ALL);
+	            ResultSet rs = stmt.getResultSet();
+	            while (rs.next()) {
+	                categories.add(new Categorie(
+	                        rs.getInt("no_categorie"),
+	                        rs.getString("libelle")
+	                ));
+	            }
+	            cnx.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            BusinessException BusinessException = new BusinessException();
+	            BusinessException.addError(ErrorCodesDAL.ERROR_SQL_SELECT);
+	            throw BusinessException;
+	        }
+	        return categories;
+	    }
+	  
+	  
+	   public boolean checkForUniqueCategorieLibelle(String libelleToCheck) throws BusinessException, SQLException {
+		   Connection cnx = ConnectionProvider.getConnection();
+	        boolean isUnique = true;
+	        try {
+	            String CHECK_IF_LIBELLE_IS_UNIQUE = "SELECT * FROM CATEGORIES WHERE libelle LIKE ?";
+	            PreparedStatement stmt = cnx.prepareStatement(CHECK_IF_LIBELLE_IS_UNIQUE);
+	            stmt.setString(1, libelleToCheck);
+	            stmt.execute();
+	            ResultSet rs = stmt.getResultSet();
+	            if (rs.next()) {
+	                isUnique = false;
+	            }
+	            cnx.close();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            BusinessException BusinessException = new BusinessException();
+	            BusinessException.addError(ErrorCodesDAL.ERROR_SQL_SELECT);
+	            throw BusinessException;
+	        }
+	        return isUnique;
+	    }
+
+	
+
+	@Override
+	public void update(Categorie var) throws BusinessException, SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	  @Override
+	    public void delete(Categorie categorie) throws BusinessException, SQLException {
+
+	    }
+
+	@Override
+	public Categorie selectById(int id) throws BusinessException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	  
+	  
+	  
+	  
+	  
+	  
 }
--- Encheres-ENI/src/main/java/fr/eni/dal/DAOArticleVendu.java
+++ Encheres-ENI/src/main/java/fr/eni/dal/DAOArticleVendu.java
@@ -9,14 +9,14 @@ import fr.eni.bo.ArticleVendu;
 import fr.eni.bo.Categorie;
 import fr.eni.bo.Utilisateur;
 
-public interface DAOArticleVendu  {
+public interface DAOArticleVendu extends DAO<ArticleVendu> {
 	void insert(ArticleVendu articleVendu) throws BusinessException, SQLException;
 	List<ArticleVendu> parcategorie(Categorie categorie) throws BusinessException, SQLException;
 	List<Integer> filterByEtat(String etat) throws BusinessException, SQLException;
 	void delete(ArticleVendu articleVendu) throws BusinessException, SQLException;
 	void fillPreparedStatement(ArticleVendu articleVendu, PreparedStatement stmt) throws SQLException;
     
-    
-    
-    
+	ArticleVendu selectById(int id) throws BusinessException, SQLException ;
+	List<ArticleVendu> selectAll() throws BusinessException, SQLException;
+	void update(ArticleVendu var) throws BusinessException, SQLException;
 }
