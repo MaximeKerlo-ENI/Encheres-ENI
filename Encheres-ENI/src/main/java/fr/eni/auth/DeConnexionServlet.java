cee4310b1eb80e51412eed487ea37356f6e2ff3d
KERLO Maxime <maxime.kerlo2022@campus-eni.fr>
2022-04-14 15:34:47 +0200

    deco

--- Encheres-ENI/src/main/java/fr/eni/auth/DeConnexionServlet.java
+++ Encheres-ENI/src/main/java/fr/eni/auth/DeConnexionServlet.java
@@ -20,8 +20,7 @@ public class DeConnexionServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// l'objet HttpSession est le même dans TOUS les servlets de l'application, mais différent pour chaque utilisateur
 		HttpSession session = request.getSession();
-		session.removeAttribute("utilisateurConnecte");  // c'est en supprimant l'attribut "utilisateurConnecte" de la session qu'on effectue la deconnexion
-		// session.invalidate(); : enlève TOUS les attributs de session 
-		response.sendRedirect("./"); // je redirige sur la page d'accueil donc pas besoin de faire de JSP pour cette fonctionnalité
+		session.invalidate();
+		response.sendRedirect("./index.jsp"); // je redirige sur la page d'accueil donc pas besoin de faire de JSP pour cette fonctionnalité
 	}
 }
