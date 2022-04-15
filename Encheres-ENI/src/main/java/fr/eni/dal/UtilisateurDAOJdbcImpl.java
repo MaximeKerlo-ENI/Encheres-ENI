acba119d258a7abfc8992a98795f1822e22c05e3
clozachmeur2021 <clozachmeur2021@170SE3-9Q405M2.ad.campus-eni.fr>
2022-04-15 11:45:46 +0200

    modi requete insert utilisateur

--- Encheres-ENI/src/main/java/fr/eni/dal/UtilisateurDAOJdbcImpl.java
+++ Encheres-ENI/src/main/java/fr/eni/dal/UtilisateurDAOJdbcImpl.java
@@ -24,6 +24,17 @@ public class UtilisateurDAOJdbcImpl implements DAOUtilisateur {
                     "(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) " +
                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement pStmt = cnx.prepareStatement(INSERT,Statement.RETURN_GENERATED_KEYS);
+            pStmt.setString(1, utilisateur.getPseudo());
+            pStmt.setString(2,utilisateur.getNom());
+            pStmt.setString(3, utilisateur.getPrenom());
+            pStmt.setString(4,utilisateur.getEmail());
+            pStmt.setString(5, utilisateur.getTelephone());
+            pStmt.setString(6, utilisateur.getRue());
+            pStmt.setString(7, utilisateur.getCodePostal());
+            pStmt.setString(8,utilisateur.getVille());
+            pStmt.setString(9,utilisateur.getMotDePasse());
+            pStmt.setInt(10,utilisateur.getCredit());
+            pStmt.setBoolean(11,utilisateur.getAdministrateur());
             pStmt.executeUpdate();
             ResultSet rs = pStmt.getGeneratedKeys();
             if (rs.next()) {
