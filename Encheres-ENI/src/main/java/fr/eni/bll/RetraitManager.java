37cc7f2c72d9cdd1aa97e8260d5840d35c81c098
Christophe RENIER <christophe.renier2022@campus-eni.fr>
2022-04-19 11:42:33 +0200

    Création RetraitManager

---
+++ Encheres-ENI/src/main/java/fr/eni/bll/RetraitManager.java
@@ -0,0 +1,50 @@
+package fr.eni.bll;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.bo.ArticleVendu;
+import fr.eni.bo.Retrait;
+import fr.eni.dal.DAOFactory;
+import fr.eni.dal.DAORetrait;
+import fr.eni.dal.DalException;
+
+public class RetraitManager {
+	
+	private DAORetrait daoRetrait = (DAORetrait) DAOFactory.getDAORetrait();
+
+	public void insert(Retrait retrait) throws DalException {
+		try {
+			this.daoRetrait.insert(retrait);
+		} catch (DalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+//	public void update(Retrait retrait) throws DalException {
+//	
+//		this.daoRetrait.insert(retrait);
+//	}
+	
+	void delete(Retrait retrait) throws DalException{
+		try {
+			this.daoRetrait.delete(retrait);
+		} catch (DalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Retrait> selectAll() throws DalException {
+		List<Retrait> listeRetrait= new ArrayList<>();
+		try {
+			listeRetrait=this.daoRetrait.selectAll();
+		} catch (DalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return listeRetrait;
+	}
+	
+}
