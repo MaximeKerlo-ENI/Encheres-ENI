4c30cabd3987a8b9c1517d7bf589a9d4614a0804
Christophe RENIER <christophe.renier2022@campus-eni.fr>
2022-04-19 11:56:49 +0200

    Création CategorieManager

---
+++ Encheres-ENI/src/main/java/fr/eni/bll/CategorieManager.java
@@ -0,0 +1,48 @@
+package fr.eni.bll;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.bo.Categorie;
+import fr.eni.dal.DAOCategorie;
+import fr.eni.dal.DAOFactory;
+import fr.eni.dal.DalException;
+
+public class CategorieManager {
+
+	private DAOCategorie daoCategorie = (DAOCategorie) DAOFactory.getDAOCategorie();
+
+	public void insert(Categorie categorie) throws DalException {
+		try {
+			this.daoCategorie.insert(categorie);
+		} catch (DalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+//	public void update(Categorie categorie) throws DalException {
+//	
+//		this.daoRetrait.insert(categorie);
+//	}
+
+	void delete(Categorie categorie) throws DalException {
+		try {
+			this.daoCategorie.delete(categorie);
+		} catch (DalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public List<Categorie> selectAll() throws DalException {
+		List<Categorie> listeCategorie = new ArrayList<>();
+		try {
+			listeCategorie = this.daoCategorie.selectAll();
+		} catch (DalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return listeCategorie;
+	}
+}
