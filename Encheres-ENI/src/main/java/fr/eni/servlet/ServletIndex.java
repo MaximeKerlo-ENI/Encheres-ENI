cde142d952b893058b97bb7ce1f8016e66e9acec
KERLO Maxime <maxime.kerlo2022@campus-eni.fr>
2022-04-19 09:50:11 +0200

    début servlet index

--- Encheres-ENI/src/main/java/fr/eni/servlet/ServletIndex.java
+++ Encheres-ENI/src/main/java/fr/eni/servlet/ServletIndex.java
@@ -1,11 +1,18 @@
 package fr.eni.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.bll.UserManager;
+import fr.eni.bo.ArticleVendu;
+import fr.eni.javaee.bll.BusinessException;
+
 
 /**
  * Servlet implementation class ServletIndex
@@ -13,6 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletIndex")
 public class ServletIndex extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private ArticleManager articleManager = new ArticleManager();
+	private ArrayList<ArticleVendu> listeArticles = new ArrayList<ArticleVendu>();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -26,8 +35,14 @@ public class ServletIndex extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			listeArticles = articleManager.selectAll();
+			request.setAttribute("listeArticles", listeArticles);		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.getRequestDispatcher("/index.jsp").forward(request, response);	
 	}
 
 	/**
